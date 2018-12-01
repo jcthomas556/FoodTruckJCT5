02e83722335e101f86aab29b0a9a4ebba0875609
Jacob <jcthomas556@byui.com>
2018-12-01 13:48:31 -0700

    Addition of a working Square

---
+++ app/src/main/java/com/example/jctho/foodtruckjct5/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.jctho.foodtruckjct5;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.squareup.sdk.pos.ChargeRequest;
+import com.squareup.sdk.pos.CurrencyCode;
+import com.squareup.sdk.pos.PosClient;
+import com.squareup.sdk.pos.PosSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String APPLICATION_ID = "sq0idp-1OcBrfMiihiS4krzlGJ2HQ";
+
+    private PosClient posClient;
+
+    // create a new charge request and initiate a Point of Sale transaction
+    private static final int CHARGE_REQUEST_CODE = 1;
+    public void startTransaction() {
+        ChargeRequest request = new ChargeRequest.Builder(
+                100,
+                CurrencyCode.USD)
+                .build();
+        try {
+            Intent intent = posClient.createChargeIntent(request);
+            startActivityForResult(intent, CHARGE_REQUEST_CODE);
+
+        } catch (ActivityNotFoundException e) {
+           /* AlertDialogHelper.showDialog( // this is needed if it ever goes to production
+                    this,
+                    "Error",
+                    "Square Point of Sale is not installed"
+            );*/
+            posClient.openPointOfSalePlayStoreListing();
+        }
+    }
+
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Handle unexpected errors
+        if (data == null || requestCode != CHARGE_REQUEST_CODE) {
+            // AlertDialogHelper.showDialog(this,
+            // "Error: unknown",
+            //"Square Point of Sale was uninstalled or stopped working");
+            return;
+        }
+
+        // Handle expected results
+        if (resultCode == Activity.RESULT_OK) {
+            // Handle success
+            //ChargeRequest.Success success = posClient.parseChargeSuccess(data);
+            //AlertDialogHelper.showDialog(this,
+            //  "Success",
+            // "Client transaction ID: "
+            //    + success.clientTransactionId);
+        } else {
+            // Handle expected errors
+            // ChargeRequest.Error error = posClient.parseChargeError(data);
+            //AlertDialogHelper.showDialog(this,
+            //  "Error" + error.code,
+            //  "Client transaction ID: "
+            //        + error.debugDescription);
+        }
+        return;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        posClient = PosSdk.createClient(this, APPLICATION_ID);//JCT SQUARE
+
+        setContentView(R.layout.activity_main);
+
+        Button sides = findViewById(R.id.button);
+        Button drinks = findViewById(R.id.button2);
+        Button sandwich = findViewById(R.id.button3);
+
+        sandwich.setOnClickListener(this);//not sure what's going on here
+        sides.setOnClickListener(this);
+        drinks.setOnClickListener(this);
+    }
+
+    public void openSandwich_select() {
+        Intent intent = new Intent( this, TestActivity1.class);
+        startActivity(intent);
+    }
+
+    public void openSide_select() {
+        Intent intent = new Intent( this, TestActivity1.class);
+        startActivity(intent);
+    }
+
+    public void openDrink_select() {
+        Intent intent = new Intent( this, TestActivity1.class);
+        startActivity(intent);
+    }
+
+
+    //@Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button3:
+                openSandwich_select();
+                break;
+            case R.id.button:
+                openSide_select();
+                break;
+            case R.id.button2:
+                openDrink_select();
+                break;
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/jctho/foodtruckjct5/TestActivity1.java
@@ -0,0 +1,173 @@
+package com.example.jctho.foodtruckjct5;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.jctho.foodtruckjct5.R;
+
+public class TestActivity1 extends AppCompatActivity implements View.OnClickListener{//sandwiches activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sandwich_select);
+
+        Button plusButton9 = findViewById(R.id.button9);//jct
+        Button minusButton5 = findViewById(R.id.button5);
+        Button plusButton10 = findViewById(R.id.button10);
+        Button minusButton6 = findViewById(R.id.button6);
+        Button plusButton12 = findViewById(R.id.button12);
+        Button minusButton7 = findViewById(R.id.button7);
+        Button plusButton13 = findViewById(R.id.button13);
+        Button minusButton8 = findViewById(R.id.button8);
+
+
+        plusButton9.setOnClickListener(this);//jct
+        minusButton5.setOnClickListener((this));
+        plusButton10.setOnClickListener((this));
+        minusButton6.setOnClickListener((this));
+        plusButton12.setOnClickListener(this);
+        minusButton7.setOnClickListener(this);
+        plusButton13.setOnClickListener(this);
+        minusButton8.setOnClickListener(this);
+    }
+
+//////////////////jct plus/minus buttons sandwich one
+
+/*
+    public void plusButtonTwo(int count, View myView){
+       count ++;
+        //I'd like to make the plus and minus methods work for all buttons, but that would require passing in a counter and a view and I'm not sure how that would work.
+
+        TextView incremental = findViewById(myView);
+    }
+*/
+
+    /**
+     * The code below is responsible for making incremental and decremental buttons.
+     * @param view
+     */
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.button9:
+                plusButtonOne(view);
+                break;
+            case R.id.button5:
+                minusButtonOne(view);
+                break;
+            case R.id.button10:
+                plusButtonTwo(view);
+                break;
+            case R.id.button6:
+                minusButtonTwo(view);
+                break;
+            case R.id.button12:
+                plusButtonThree(view);
+                break;
+            case R.id.button7:
+                minusButtonThree(view);
+                break;
+            case R.id.button13:
+                plusButtonFour(view);
+                break;
+            case R.id.button8:
+                minusButtonFour(view);
+                break;
+        }
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////button 1
+    int countButton9and5 = 0;
+    public void plusButtonOne(View view){
+        countButton9and5 ++;
+
+        TextView sandwichIncrementer1 = findViewById(R.id.textView5);
+        sandwichIncrementer1.setText("" + countButton9and5);
+
+    }
+    public void minusButtonOne(View view){//make sure the counter doesn't go negative
+        if (countButton9and5 > 0){
+            countButton9and5 --;
+        }
+        else {
+            return;
+        }
+
+        TextView sandwichIncrementer1 = findViewById(R.id.textView5);
+        sandwichIncrementer1.setText("" + countButton9and5);
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////button 2
+    int countButton10and6=0;
+    public void plusButtonTwo(View view){
+        countButton10and6 ++;
+
+        TextView sandwichIncrementer2 = findViewById(R.id.textView6);
+        sandwichIncrementer2.setText("" + countButton10and6);
+
+    }
+    public void minusButtonTwo(View view){//make sure the counter doesn't go negative
+        if (countButton10and6 > 0){
+            countButton10and6 --;
+        }
+        else {
+            return;
+        }
+
+        TextView sandwichIncrementer2 = findViewById(R.id.textView6);
+        sandwichIncrementer2.setText("" + countButton10and6);
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////button 3
+
+    int countButton12and7=0;
+    public void plusButtonThree(View view){
+        countButton12and7 ++;
+
+        TextView sandwichIncrementer3 = findViewById(R.id.textView7);
+        sandwichIncrementer3.setText("" + countButton12and7);
+
+    }
+    public void minusButtonThree(View view){//make sure the counter doesn't go negative
+        if (countButton12and7 > 0){
+            countButton12and7 --;
+        }
+        else {
+            return;
+        }
+
+        TextView sandwichIncrementer3 = findViewById(R.id.textView7);
+        sandwichIncrementer3.setText("" + countButton12and7);
+    }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////button 4
+
+
+    int countButton13and8=0;
+    public void plusButtonFour(View view){
+        countButton13and8 ++;
+
+        TextView sandwichIncrementer1 = findViewById(R.id.textView8);
+        sandwichIncrementer1.setText("" + countButton13and8);
+
+    }
+    public void minusButtonFour(View view){//make sure the counter doesn't go negative
+        if (countButton13and8 > 0){
+            countButton13and8 --;
+        }
+        else {
+            return;
+        }
+
+        TextView sandwichIncrementer4 = findViewById(R.id.textView8);
+        sandwichIncrementer4.setText("" + countButton13and8);
+    }
+
+
+////////////////jct end
+
+}
