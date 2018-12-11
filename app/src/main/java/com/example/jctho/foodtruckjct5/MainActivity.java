bc48f52009a652ef3034679440574bea1f404add
Jacob <jcthomas556@byui.com>
2018-12-10 18:30:53 -0700

    Jacob 4 - Includes the start of the plus and minus buttons, I also added the code for the adding of each items to the array list.

--- app/src/main/java/com/example/jctho/foodtruckjct5/MainActivity.java
+++ app/src/main/java/com/example/jctho/foodtruckjct5/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.squareup.sdk.pos.ChargeRequest;
 import com.squareup.sdk.pos.CurrencyCode;
@@ -18,9 +19,9 @@ import com.squareup.sdk.pos.PosSdk;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ArrayList<Item> order = new ArrayList<>();
+    //public static ArrayList<Item> order = new ArrayList<>();//JCT uncomment when buttons are put here instead
     private long animationDuration = 500;
     private static final String APPLICATION_ID = "sq0idp-1OcBrfMiihiS4krzlGJ2HQ";
     private ConstraintLayout sandView = null;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // create a new charge request and initiate a Point of Sale transaction
     private static final int CHARGE_REQUEST_CODE = 1;
+
     public void startTransaction() {
         ChargeRequest request = new ChargeRequest.Builder(
                 100,
@@ -49,7 +51,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         // Handle unexpected errors
         if (data == null || requestCode != CHARGE_REQUEST_CODE) {
@@ -92,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         Display display = getWindowManager().getDefaultDisplay();
-        Point size= new Point();
+        Point size = new Point();
         display.getSize(size);
         sandView.setX(size.x);
         drinksView.setX(size.x);
@@ -105,61 +108,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button sandBack = findViewById(R.id.sandback);
         Button drinkBack = findViewById(R.id.drinkback);
         Button sidesBack = findViewById(R.id.sidesback);
+        Button addReuben = findViewById(R.id.button16);/////////////jct
+        Button plusButtonReuben = findViewById((R.id.button15));/////////////jct
 
         sandBack.setOnClickListener(this);
-        sandwich.setOnClickListener(this);//not sure what's going on here
+        sandwich.setOnClickListener(this);
         sides.setOnClickListener(this);
         drinks.setOnClickListener(this);
         drinkBack.setOnClickListener(this);
         sidesBack.setOnClickListener(this);
+
+
+        plusButtonReuben.setOnClickListener(this);/////////////jct
+        addReuben.setOnClickListener(this);///////////////////////jct
+
+
+
     }
 
-    public void hideSandwhich(View view){
+    public void hideSandwhich(View view) {
         sandView.animate().x(sandView.getWidth()).setDuration(animationDuration);
     }
 
-    public void nothing(View view){
+    public void nothing(View view) {
     }
 
-    public void showSandwhich(View view){
+    public void showSandwhich(View view) {
         sandView.animate().x(0).setDuration(animationDuration);
     }
 
-    public void hideDrinks(View view){
+    public void hideDrinks(View view) {
         drinksView.animate().x(drinksView.getWidth()).setDuration(animationDuration);
     }
 
-    public void showDrinks(View view){
+    public void showDrinks(View view) {
         drinksView.animate().x(0).setDuration(animationDuration);
     }
 
-    public void hideSides(View view){
+    public void hideSides(View view) {
         sidesView.animate().x(sidesView.getWidth()).setDuration(animationDuration);
     }
 
-    public void showSides(View view){
+    public void showSides(View view) {
         sidesView.animate().x(0).setDuration(animationDuration);
     }
 
     public void openSandwich_select() {
-        Intent intent = new Intent( this, TestActivity1.class);
+        Intent intent = new Intent(this, TestActivity1.class);
         startActivity(intent);
     }
 
     public void openSide_select() {
-        Intent intent = new Intent( this, TestActivity1.class);
+        Intent intent = new Intent(this, TestActivity1.class);
         startActivity(intent);
     }
 
     public void openDrink_select() {
-        Intent intent = new Intent( this, TestActivity1.class);
+        Intent intent = new Intent(this, TestActivity1.class);
         startActivity(intent);
     }
 
 
     //@Override
     public void onClick(View v) {
-        switch(v.getId()) {
+        switch (v.getId()) {
             case R.id.button3:
                 showSandwhich(sandView);
                 break;
@@ -178,8 +190,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.drinkback:
                 hideDrinks(drinksView);
                 break;
+            case R.id.button16:
+                addReubon(sandView);//added sandView
+                break;
+            case R.id.button15:
+                plusButtonOne(sandView);//keep putting sandView
+                break;
         }
 
 
     }
+
+
+    /////////////////////////////////////////////////////////////////////////////////plus and minus buttons
+
+    int countButton14and15 = 0;
+
+    public void plusButtonOne(View view) {
+        countButton14and15++;
+
+        TextView sandwichIncrementer = findViewById(R.id.textView9);
+        sandwichIncrementer.setText("" + countButton14and15);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ////////////////JCT
+    class Item {
+        private String name = "default";
+        private float price = 0;
+    }
+
+    private ArrayList order = new ArrayList<>();//temporary use in this class
+
+    public void addReubon(View view) {
+        TextView reubenCounter = findViewById(R.id.textView9);////jct
+        int itemQuantity = Integer.valueOf(reubenCounter.getText().toString());
+
+        Item reubenOne = new Item();
+        reubenOne.name = "Reuben";
+        reubenOne.price = 500;///vs "$500"?
+
+        if (order.contains(reubenOne)) {
+            order.remove(reubenOne);
+
+            for (int x = 0; x <= itemQuantity; x++) {
+                order.add(reubenOne);
+
+            }
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/jctho/foodtruckjct5/TestActivity1.java
+++ app/src/main/java/com/example/jctho/foodtruckjct5/TestActivity1.java
@@ -1,3 +1,4 @@
+
 package com.example.jctho.foodtruckjct5;
 
 import android.os.Bundle;
@@ -8,7 +9,9 @@ import android.widget.TextView;
 
 import com.example.jctho.foodtruckjct5.R;
 
-public class TestActivity1 extends AppCompatActivity implements View.OnClickListener{//sandwiches activity
+import java.util.ArrayList;
+
+public class TestActivity1 extends AppCompatActivity implements View.OnClickListener {//sandwiches activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,8 @@ public class TestActivity1 extends AppCompatActivity implements View.OnClickList
         Button plusButton13 = findViewById(R.id.button13);
         Button minusButton8 = findViewById(R.id.button8);
 
+        Button reubonDone = findViewById(R.id.button16);
+
 
         plusButton9.setOnClickListener(this);//jct
         minusButton5.setOnClickListener((this));
@@ -33,25 +38,27 @@ public class TestActivity1 extends AppCompatActivity implements View.OnClickList
         minusButton7.setOnClickListener(this);
         plusButton13.setOnClickListener(this);
         minusButton8.setOnClickListener(this);
+
+
+        reubonDone.setOnClickListener(this);
     }
 
-//////////////////jct plus/minus buttons sandwich one
 
-/*
-    public void plusButtonTwo(int count, View myView){
-       count ++;
-        //I'd like to make the plus and minus methods work for all buttons, but that would require passing in a counter and a view and I'm not sure how that would work.
+    //item class
 
-        TextView incremental = findViewById(myView);
+    class Item {
+        private String name;
+        private float price;
     }
-*/
+
 
     /**
      * The code below is responsible for making incremental and decremental buttons.
+     *
      * @param view
      */
-    public void onClick(View view){
-        switch(view.getId()){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.button9:
                 plusButtonOne(view);
                 break;
@@ -76,44 +83,50 @@ public class TestActivity1 extends AppCompatActivity implements View.OnClickList
             case R.id.button8:
                 minusButtonFour(view);
                 break;
+            case R.id.button16:
+                addReubon(view);
+                break;
         }
     }
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////button 1
     int countButton9and5 = 0;
-    public void plusButtonOne(View view){
-        countButton9and5 ++;
+
+    public void plusButtonOne(View view) {
+        countButton9and5++;
 
         TextView sandwichIncrementer1 = findViewById(R.id.textView5);
         sandwichIncrementer1.setText("" + countButton9and5);
 
     }
-    public void minusButtonOne(View view){//make sure the counter doesn't go negative
-        if (countButton9and5 > 0){
-            countButton9and5 --;
-        }
-        else {
+
+    public void minusButtonOne(View view) {//make sure the counter doesn't go negative
+        if (countButton9and5 > 0) {
+            countButton9and5--;
+        } else {
             return;
         }
 
         TextView sandwichIncrementer1 = findViewById(R.id.textView5);
         sandwichIncrementer1.setText("" + countButton9and5);
     }
+
     ////////////////////////////////////////////////////////////////////////////////////////////////button 2
-    int countButton10and6=0;
-    public void plusButtonTwo(View view){
-        countButton10and6 ++;
+    int countButton10and6 = 0;
+
+    public void plusButtonTwo(View view) {
+        countButton10and6++;
 
         TextView sandwichIncrementer2 = findViewById(R.id.textView6);
         sandwichIncrementer2.setText("" + countButton10and6);
 
     }
-    public void minusButtonTwo(View view){//make sure the counter doesn't go negative
-        if (countButton10and6 > 0){
-            countButton10and6 --;
-        }
-        else {
+
+    public void minusButtonTwo(View view) {//make sure the counter doesn't go negative
+        if (countButton10and6 > 0) {
+            countButton10and6--;
+        } else {
             return;
         }
 
@@ -123,19 +136,20 @@ public class TestActivity1 extends AppCompatActivity implements View.OnClickList
 
 ////////////////////////////////////////////////////////////////////////////////////////////////button 3
 
-    int countButton12and7=0;
-    public void plusButtonThree(View view){
-        countButton12and7 ++;
+    int countButton12and7 = 0;
+
+    public void plusButtonThree(View view) {
+        countButton12and7++;
 
         TextView sandwichIncrementer3 = findViewById(R.id.textView7);
         sandwichIncrementer3.setText("" + countButton12and7);
 
     }
-    public void minusButtonThree(View view){//make sure the counter doesn't go negative
-        if (countButton12and7 > 0){
-            countButton12and7 --;
-        }
-        else {
+
+    public void minusButtonThree(View view) {//make sure the counter doesn't go negative
+        if (countButton12and7 > 0) {
+            countButton12and7--;
+        } else {
             return;
         }
 
@@ -147,19 +161,20 @@ public class TestActivity1 extends AppCompatActivity implements View.OnClickList
 ////////////////////////////////////////////////////////////////////////////////////////////////////button 4
 
 
-    int countButton13and8=0;
-    public void plusButtonFour(View view){
-        countButton13and8 ++;
+    int countButton13and8 = 0;
+
+    public void plusButtonFour(View view) {
+        countButton13and8++;
 
         TextView sandwichIncrementer1 = findViewById(R.id.textView8);
         sandwichIncrementer1.setText("" + countButton13and8);
 
     }
-    public void minusButtonFour(View view){//make sure the counter doesn't go negative
-        if (countButton13and8 > 0){
-            countButton13and8 --;
-        }
-        else {
+
+    public void minusButtonFour(View view) {//make sure the counter doesn't go negative
+        if (countButton13and8 > 0) {
+            countButton13and8--;
+        } else {
             return;
         }
 
@@ -169,5 +184,27 @@ public class TestActivity1 extends AppCompatActivity implements View.OnClickList
 
 
 ////////////////jct end
+
+
+    private ArrayList<Item> order = new ArrayList<>();//temporary use in this class
+
+    public void addReubon(View view) {
+        TextView sandwichIncrementer1 = findViewById(R.id.textView5);
+        int itemQuantity = Integer.valueOf(sandwichIncrementer1.getText().toString());
+
+        Item reubenOne = new Item();
+
+
+        if (order.contains(reubenOne))
+        {
+            order.remove(reubenOne);
+            for (int x = 0; x <= itemQuantity; x++) {
+                order.add(reubenOne);
+            }
+
+        }
+    }
+//sandwichIncrementer1.setText("" + countButton9and5);
+
 
 }
